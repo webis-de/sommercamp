4e9c4d83d541ecc3dc3005b7aefb0bcefc4461b6
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-01-22 19:30:08 +0000

    Add crawler

---
+++ sommercamp/crawler.py
@@ -0,0 +1,43 @@
+from typing import Any, Iterator
+
+from resiliparse.extract.html2text import extract_plain_text
+from scrapy import Spider, Request
+from scrapy.linkextractors.lxmlhtml import \
+    LxmlLinkExtractor, IGNORED_EXTENSIONS
+from scrapy.http.response import Response
+
+
+class SchoolSpider(Spider):
+    name: str = "school"
+
+    start_urls: list[str] = [
+        "https://wilhelm-gym.de/",
+    ]
+    link_extractor = LxmlLinkExtractor(
+        allow_domains=[
+            "wilhelm-gym.de",
+        ],
+        deny_extensions=[
+            *IGNORED_EXTENSIONS,
+            "webp",
+        ]
+    )
+    custom_settings = {
+        "USER_AGENT": "Sommercamp (https://sommercamp.uni-jena.de)",
+        "ROBOTSTXT_OBEY": True,
+        "CONCURRENT_REQUESTS": 4,
+        "AUTOTHROTTLE_ENABLED": True,
+        "AUTOTHROTTLE_TARGET_CONCURRENCY": 1,
+        "HTTPCACHE_ENABLED": True,
+    }
+
+    def parse(self, response: Response) -> Iterator[dict[str, Any]]:
+
+        yield {
+            "url": response.url,
+            "title": response.css("title::text").get(),
+            "text": extract_plain_text(response.text, main_content=True),
+        }
+
+        for link in self.link_extractor.extract_links(response):
+            yield Request(link.url, callback=self.parse)
