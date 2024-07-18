233fa9bdd5fdebcf9015dad650dd6ee8e3815d43
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-07-18 13:04:58 +0200

    Simplify and document crawler

--- sommercamp/crawler.py
+++ sommercamp/crawler.py
@@ -1,45 +1,59 @@
-from typing import Any, Iterator
-from uuid import uuid5, NAMESPACE_URL
-
+# Hier importieren wir die benötigten Softwarebibliotheken.
 from resiliparse.extract.html2text import extract_plain_text
 from scrapy import Spider, Request
-from scrapy.linkextractors.lxmlhtml import \
-    LxmlLinkExtractor, IGNORED_EXTENSIONS
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor, IGNORED_EXTENSIONS
 from scrapy.http.response import Response
 
 
 class SchoolSpider(Spider):
-    name: str = "school"
+    # Gib hier dem Crawler einen eindeutigen Name,
+    # der beschreibt, was du crawlst.
+    name = "school"
 
-    start_urls: list[str] = [
+    start_urls = [
+        # Gib hier mindestens eine (oder mehrere) URLs an,
+        # bei denen der Crawler anfangen soll,
+        # Seiten zu downloaden.
         "https://wilhelm-gym.de/",
     ]
     link_extractor = LxmlLinkExtractor(
-        allow_domains=[
-            "wilhelm-gym.de",
-        ],
-        deny_extensions=[
-            *IGNORED_EXTENSIONS,
-            "webp",
-        ]
+        # Beschränke den Crawler, nur Links zu verfolgen,
+        # die auf eine der gelisteten Domains verweisen.
+        allow_domains=["wilhelm-gym.de"],
+        # Ignoriere Links mit bestimmten Datei-Endungen.
+        deny_extensions=[*IGNORED_EXTENSIONS, "webp"],
     )
     custom_settings = {
-        "USER_AGENT": "Sommercamp (https://sommercamp.uni-jena.de)",
+        # Identifiziere den Crawler gegenüber den gecrawlten Seiten.
+        "USER_AGENT": "Sommercamp (https://uni-jena.de)",
+        # Der Crawler soll nur Seiten crawlen, die das auch erlauben.
         "ROBOTSTXT_OBEY": True,
+        # Frage zu jeder Zeit höchstens 4 Webseiten gleichzeitig an.
         "CONCURRENT_REQUESTS": 4,
+        # Verlangsame den Crawler, wenn Webseiten angeben,
+        # dass sie zu oft angefragt werden.
         "AUTOTHROTTLE_ENABLED": True,
         "AUTOTHROTTLE_TARGET_CONCURRENCY": 1,
+        # Frage nicht zwei mal die selbe Seite an.
         "HTTPCACHE_ENABLED": True,
     }
 
-    def parse(self, response: Response) -> Iterator[dict[str, Any]]:
-
+    def parse(self, response):
+        # Speichere die Webseite als ein Dokument in unserer Dokumentensammlung.
         yield {
-            "docno": str(uuid5(NAMESPACE_URL, response.url)),
+            # Eine eindeutige Identifikations-Nummer für das Dokument.
+            "docno": str(hash(response.url)),
+            # Die URL der Webseite.
             "url": response.url,
+            # Der Titel der Webseite aus dem <title> Tag im HTML-Code.
             "title": response.css("title::text").get(),
+            # Der Text der Webseite.
+            # Um den Hauptinhalt zu extrahieren, benutzen wir
+            # eine externe Bibliothek.
             "text": extract_plain_text(response.text, main_content=True),
         }
 
+        # Finde alle Links auf der aktuell betrachteten Webseite.
         for link in self.link_extractor.extract_links(response):
+            # Für jeden gefundenen Link, stelle eine Anfrage zum Crawling.
             yield Request(link.url, callback=self.parse)
