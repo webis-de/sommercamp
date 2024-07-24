7671560c721d3d0979b4b5df775fecd708704e87
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-07-24 22:44:20 +0200

    Fix stride

--- .teachers/app_snippets.py
+++ .teachers/app_snippets.py
@@ -51,7 +51,7 @@ def app(index_dir) -> None:
     text_getter = get_text(index, metadata=["url", "title", "text"])
 
     # Initialisiere das Modul, zum Aufteilen der Texte in 15-Wort-Schnipsel.
-    snippets_splitter = sliding(text_attr="text", length=15, prepend_attr=None)
+    snippets_splitter = sliding(text_attr="text", length=15, stride=5, prepend_attr=None)
     # Initialisiere das Modul, zum Ranking der Text-Schnipsel.
     snippets_scorer = scorer(body_attr="text", wmodel="Tf", takes="docs")
     # Initialisiere das Modul, zum Zusammenfügen der Text-Schnipsel zu Snippets.
